956c80a6d00843e26ec08051f53202c53b3ecea9
space-cadet-max <maxwelle+onboarding@spacelift.io>
2024-09-17 11:36:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 resource "null_resource" "example" {
   provisioner "local-exec" {
-    command = "echo Hdeedllo, Worlldasdffasdfasfdddld!"
+    command = "echo hHdeedllo, Worlldasdffasdfasfdddld!"
   }
 }
