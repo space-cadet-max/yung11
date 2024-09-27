8242f77964be84a124ce03b4bd4e14d7dab2bb6b
space-cadet-max <maxwelle+onboarding@spacelift.io>
2024-09-27 17:12:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "test_colab_bucket" {
+resource "aws_s3_bucket" "test_coladb_bucket" {
   bucket = "test-colab"
   acl    = "private"
 
